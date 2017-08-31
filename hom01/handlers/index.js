5d4ee2b592cf519f9c7b65888bd91d42f936417a
Vanya Ivanova <van4eto_986@abv.bg>
2017-08-31 14:15:47 +0300

    2.0.1

--- hom01/handlers/index.js
+++
@@ -1,5 +0,0 @@
-const homeHandler = require('./home')
-const filesHandler = require('./static-files')
-const products = require('./product')
-
-module.exports = [homeHandler, filesHandler, products]
